b8c6dab2f257971132f1f2da4589b23fcd310d8b
AZR88 <azrielamaldany@gmail.com>
2024-08-29 23:48:09 +0700

    Update Path test

--- src/test/java/page/loginpage.java
+++ src/test/java/page/loginpage.java
@@ -1,22 +1,23 @@
+package page;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class Login_Page {
+public class loginpage {
+
     @Test
     public void UI() throws InterruptedException {
         WebDriver driver = WebDriverManager.edgedriver().create();
         driver.get("https://www.demoblaze.com/");
         driver.manage().window().maximize();
 
-        // Melakukan tindakan-tindakan pada halaman web
         driver.findElement(By.id("login2")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#loginusername")));
@@ -26,10 +27,8 @@ public class Login_Page {
 
         driver.findElement(By.cssSelector("button[onclick='logIn()']")).click();
 
-        // Menunggu sebentar untuk melihat hasilnya
-        Thread.sleep(2000);
+        Thread.sleep(5000);
 
-        // Tutup browser setelah selesai
         driver.quit();
     }
 }
