07cf92bef6c8340339bc128d4799edfaf2b3f973
AZR88 <azrielamaldany@gmail.com>
2024-09-07 23:26:10 +0700

    Add signup Path

--- src/test/java/page/SignUp_Page.java
+++ src/test/java/page/SignUp_Page.java
@@ -1,9 +1,14 @@
 package page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignUp_Page {
-    public void SignUp(){
-
-    }
+    public static By SignUpButton = By.id("signin2");
+    public static  By SignUpusernameInput = By.id("sign-username");
+    public static  By SignUppasswordInput = By.id("sign-password");
+    public static  By SubmitButton = By.xpath("button[onclick='register()']");
 }
+
+
+
