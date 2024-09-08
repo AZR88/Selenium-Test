da7ee63807554e97c5b38e8bdcb494c44e8749d8
AZR88 <azrielamaldany@gmail.com>
2024-09-08 21:27:31 +0700

    update SignUpTest.java

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class SignUpTest {
+
+    public static WebDriver driver;
+
+    @BeforeEach
+    public void setDriver(){
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.demoblaze.com/");
+    }
 }
