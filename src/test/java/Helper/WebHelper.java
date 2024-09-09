152cd41e21d0948e96f7f730fb9a79ba289afdbd
AZR88 <azrielamaldany@gmail.com>
2024-09-09 23:06:42 +0700

    add helper

---
+++ src/test/java/Helper/WebHelper.java
@@ -0,0 +1,27 @@
+package Helper;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class WebHelper {
+
+    public static WebDriver driver;
+
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.demoblaze.com/");
+    }
+
+    public void tearDown(){driver.quit();}
+}
