6873e35c9469f3b1d32ad778f0607dcb712394ca
AZR88 <azrielamaldany@gmail.com>
2024-09-09 23:02:12 +0700

    fix step def

---
+++ src/test/java/StepDef/LoginStepDef.java
@@ -0,0 +1,15 @@
+package StepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+    @Given("user is on homepage")
+    public void userIsOnHomepage() {
+        
+    }
+
+    @When("user click Login button")
+    public void userClickLoginButton() {
+    }
+}
--- src/test/resources/LoginStepDef.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.java.en.Given;
-
-public class LoginStepDef {
-    @Given("user is on homepage")
-    public void userIsOnHomepage() {
-    }
-}
