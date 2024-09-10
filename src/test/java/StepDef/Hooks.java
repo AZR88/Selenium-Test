b0935962ae7b6e7c3ac11ae63dc512f5545ea1cb
AZR88 <azrielamaldany@gmail.com>
2024-09-10 20:22:45 +0700

    update Hooks

--- src/test/java/StepDef/Hooks.java
+++ src/test/java/StepDef/Hooks.java
@@ -1,4 +1,33 @@
 package StepDef;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static Helper.WebHelper.startDriver;
+import static Helper.WebHelper.tearDown;
+
+
 public class Hooks {
+
+    @BeforeAll
+    public static void startUp (){
+
+    }
+
+    @AfterAll
+    public static void shutDown (){
+
+    }
+
+    @Before
+    public void beforeTest (){
+        startDriver();
+    }
+
+    @After
+    public  void afterTest (){
+        tearDown();
+    }
 }
