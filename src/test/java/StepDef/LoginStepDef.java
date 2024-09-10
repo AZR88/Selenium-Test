687500cd10c655428839a2ac67730c5a69265cd7
AZR88 <azrielamaldany@gmail.com>
2024-09-10 20:30:37 +0700

    gradle update

--- src/test/java/StepDef/LoginStepDef.java
+++ src/test/java/StepDef/LoginStepDef.java
@@ -2,7 +2,11 @@ package StepDef;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import page.Login_Page;
+
+import java.time.Duration;
 
 import static Helper.WebHelper.driver;
 
@@ -10,7 +14,16 @@ import static Helper.WebHelper.driver;
 public class LoginStepDef {
     @Given("user is on homepage")
     public void userIsOnHomepage() {
-        
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.demoblaze.com/");
     }
 
     @When("user click Login button")
