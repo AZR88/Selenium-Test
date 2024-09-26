b5765c7eca709b5960a9b9731dd4a807020c3e04
AZR88 <azrielamaldany@gmail.com>
2024-09-26 22:04:56 +0700

    Helper update

--- src/test/java/Helper/WebHelper.java
+++ src/test/java/Helper/WebHelper.java
@@ -12,7 +12,7 @@ public class WebHelper {
 
     public static void startDriver(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
