89fc071b456c811beb930a09bc01e4e6f86624d2
AZR88 <azrielamaldany@gmail.com>
2024-10-13 19:33:12 +0700

    update feature

--- src/test/java/stepDef/SignUpStepDef.java
+++ src/test/java/stepDef/SignUpStepDef.java
@@ -1,5 +1,6 @@
 package stepDef;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import static Helper.WebHelper.driver;
 import static page.SignUp_Page.SignUpButton;
@@ -8,5 +9,9 @@ public class SignUpStepDef {
     @When("user click Signup button")
     public void userClickSignupButton() {
         driver.findElement(SignUpButton).click();
+    }
+
+    @And("user input username with \\{sosososo}")
+    public void userInputUsernameWithSosososo() {
     }
 }
