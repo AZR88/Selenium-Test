a6994c7808e9778b306da3a7ea9cf8d893501a8b
AZR88 <azrielamaldany@gmail.com>
2024-12-13 21:27:43 +0700

    update Login_Page

--- src/test/java/page/SignUp_Page.java
+++ src/test/java/page/SignUp_Page.java
@@ -58,38 +58,22 @@ public class SignUp_Page {
     }
 
     public static void verifySignupAlert(WebDriver driver) {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            try {
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 
-            // Mengambil teks dari alert
-            String alertText = alert.getText();
-            System.out.println("Teks notifikasi login invalid: " + alertText);
+                // If the alert is present, it will be printed and accepted
+                System.out.println("Alert muncul: " + alert.getText());
+                alert.accept();
 
-            // Memverifikasi teks alert sesuai yang diharapkan
-            boolean isAlertValid = false;
-
-            if (alertText.equals("User does not exist.")) {
-                System.out.println("Alert menunjukkan: 'User does not exist.'");
-                isAlertValid = true; // Menandai alert valid
-            } else if (alertText.equals("Wrong password.")) {
-                System.out.println("Alert menunjukkan: 'Wrong password.'");
-                isAlertValid = true; // Menandai alert valid
-            } else {
-                System.out.println("Notifikasi login invalid tidak sesuai: " + alertText);
+            } catch (TimeoutException e) {
+                System.out.println("Alert tidak muncul dalam waktu yang ditentukan.");
+                Assert.fail("Alert tidak muncul dalam waktu yang ditentukan.");
             }
-
-            alert.accept(); // Menutup alert
-
-            // Memverifikasi hasil menggunakan assertTrue
-            Assert.assertTrue("Notifikasi login tidak sesuai.", isAlertValid);
-
-        } catch (TimeoutException e) {
-            System.out.println("Alert tidak muncul dalam waktu yang ditentukan.");
-            Assert.fail("Alert tidak muncul dalam waktu yang ditentukan."); // Menghasilkan kegagalan assertion
         }
+
     }
-}
+
 
 
 
