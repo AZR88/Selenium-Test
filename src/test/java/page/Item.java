f42dc866e50bb04f5b9224383e7a1ae61ac1d2a4
AZR88 <azrielamaldany@gmail.com>
2024-12-18 16:37:26 +0700

    update item.java: add fuction

--- src/test/java/page/Item.java
+++ src/test/java/page/Item.java
@@ -1,6 +1,13 @@
 package page;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Item {
     private static By HomeButton = By.xpath("//li[@class='nav-item active']//a[@class='nav-link']");
@@ -21,5 +28,29 @@ public class Item {
     }
     private static By getProductPriceLocator(String expectedPrice, String productId) {
         return By.xpath("//a[contains(@href, 'prod.html?idp_=" + productId + "')]/following::h5[contains(text(), '" + expectedPrice + "')]");
+    }
+
+    public static void UrlCheck(WebDriver driver, String expectedUrl) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        Assert.assertEquals(expectedUrl, driver.getCurrentUrl());
+    }
+
+    public static boolean isTitleAndPriceTextEqual(WebDriver driver, String productId, String expectedTitle, String expectedPrice) {
+
+        By productTitleLocator = getProductTitleLocatorById(productId, expectedTitle);
+        By productPriceLocator = getProductPriceLocator(expectedPrice, productId);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productTitleLocator));
+        String actualTitle = titleElement.getText();
+
+
+        WebElement priceElement = wait.until(ExpectedConditions.visibilityOfElementLocated(productPriceLocator));
+        String actualPrice = priceElement.getText();
+
+
+        return actualTitle.equals(expectedTitle) && actualPrice.equals(expectedPrice);
     }
 }
