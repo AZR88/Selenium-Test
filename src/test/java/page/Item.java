c36a3c0831d5a209bda81979d24fa99e7513a00e
AZR88 <azrielamaldany@gmail.com>
2024-12-18 16:36:12 +0700

    update item.java

--- src/test/java/page/Item.java
+++ src/test/java/page/Item.java
@@ -1,4 +1,20 @@
 package page;
 
+import org.openqa.selenium.By;
+
 public class Item {
+    private static By HomeButton = By.xpath("//li[@class='nav-item active']//a[@class='nav-link']");
+    private static By ContactButton = By.xpath("//a[normalize-space()='Contact']");
+    private static By AboutUsButton = By.xpath("//a[normalize-space()='About us']");
+    private static By CartButton = By.xpath("(//a[normalize-space()='Cart'])");
+    private static By LoginButton = By.id("login2");
+    private static By SignUpButton = By.id("signin2");
+
+
+    private  static By nextbutton = By.id("next2");
+    private  static By PhoneCat = By.xpath("//a[@onclick=\"byCat('phone')\"]");
+    private  static By LaptopCat = By.xpath("//a[@onclick=\"byCat('notebook')\"]");
+    private  static By MonitorCat = By.xpath("//a[@onclick=\"byCat('monitor')\"]");
+
+
 }
