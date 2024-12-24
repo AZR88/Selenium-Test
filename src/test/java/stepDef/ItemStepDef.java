4089683555dcedafde97ea5cc60e940b0ea2c7df
AZR88 <azrielamaldany@gmail.com>
2024-12-25 00:53:53 +0700

    update ItemStepDef

--- src/test/java/stepDef/ItemStepDef.java
+++ src/test/java/stepDef/ItemStepDef.java
@@ -11,5 +11,30 @@ public class ItemStepDef {
     public void checkNavbarButton(String arg0) {
         CheckHomebutton(driver,arg0);
     }
+    @Then("Check {string} button")
+    public void checkButton(String arg1) {
+        CheckAboutbutton(driver,arg1);
 
+    }
+
+    @Then("Check the {string} button")
+    public void checkTheButton(String arg0) {
+        CheckCartbutton(driver,arg0);
+    }
+
+
+    @Then("Check a {string} button")
+    public void checkAButton(String arg0) {
+        CheckLoginbutton(driver,arg0);
+    }
+
+    @Then("check {string} buton")
+    public void checkButon(String arg0) {
+        CheckSignupbutton(driver,arg0);
+    }
+
+    @Then("Chek {string} button")
+    public void chekButton(String arg0) {
+        Checkcontactbutton(driver,arg0);
+    }
 }
