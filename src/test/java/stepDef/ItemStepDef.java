e053207a33ed19622f950b3ab7ed75755c4157a6
AZR88 <azrielamaldany@gmail.com>
2024-12-25 00:56:38 +0700

    update ItemStepDef

--- src/test/java/stepDef/ItemStepDef.java
+++ src/test/java/stepDef/ItemStepDef.java
@@ -1,9 +1,14 @@
 package stepDef;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+import java.util.Map;
+
 import static Helper.WebHelper.driver;
+import static org.junit.Assert.assertTrue;
 import static page.Item.*;
 
 public class ItemStepDef {
@@ -37,4 +42,32 @@ public class ItemStepDef {
     public void chekButton(String arg0) {
         Checkcontactbutton(driver,arg0);
     }
+
+    @When("check multiple titles and ids")
+    public void checkMultipleTitlesAndIds(DataTable dataTable) {
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> row : data) {
+            String productId = row.get("id");
+            String expectedTitle = row.get("title");
+            String expectedPrice = row.get("price");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            boolean isTitleAndPriceCorrect = page.Item.isTitleAndPriceTextEqual(driver, productId, expectedTitle, expectedPrice);
+
+
+            System.out.println("ID Produk: " + productId);
+            System.out.println("Expected Title: " + expectedTitle);
+            System.out.println("Expected Price: " + expectedPrice);
+            System.out.println("Actual Title and Price: " + (isTitleAndPriceCorrect ? "Sesuai" : "Tidak Sesuai"));
+
+            assertTrue("Title atau Price tidak sesuai untuk produk dengan ID " + productId, isTitleAndPriceCorrect);
+        }
+    }
+
 }
