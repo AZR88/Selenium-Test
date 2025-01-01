2ec7690f8df9fd65a51d8189d0d7711120019c57
AZR88 <azrielamaldany@gmail.com>
2025-01-01 23:07:38 +0700

    update Product_Page

--- src/test/java/page/Product_Page.java
+++ src/test/java/page/Product_Page.java
@@ -4,9 +4,17 @@ import org.openqa.selenium.By;
 
 public class Product_Page {
 
-    public static By ProductPrice = By.xpath("");
-    public static By ProductTitle = By.xpath("");
-    public static By ProductDesc = By.xpath("");
+
     public static By ProductPic = By.xpath("");
     public static By AddToCart= By.xpath("");
+
+    public static By ProductPrice = By.xpath("");
+
+    public static By ProductTitle(String productTitle) {
+        return By.xpath("//h2[@class='name' and text()='"+productTitle +"']");
+    }
+    public static By ProductDesc(String productdesc) {
+        return By.xpath("//p[contains(text(),'"+ productdesc +"')]");
+    }
+
 }
