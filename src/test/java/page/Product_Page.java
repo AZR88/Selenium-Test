671cca6e60ddf2596d777312a70a4c1a8bcd2f3f
AZR88 <azrielamaldany@gmail.com>
2025-01-01 23:20:29 +0700

    update Product_Page

--- src/test/java/page/Product_Page.java
+++ src/test/java/page/Product_Page.java
@@ -1,6 +1,13 @@
 package page;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Product_Page {
 
@@ -12,6 +19,14 @@ public class Product_Page {
 
     public static By ProductDesc(String productdesc) {
         return By.xpath("//p[contains(text(),'"+ productdesc +"')]");
+    }
+
+    public static void checkTitle(WebDriver driver, String Title) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement ItemTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//your/xpath/here")));
+
+        Assert.assertTrue(ItemTitle.isEnabled());
+        Assert.assertEquals(Title, ItemTitle.getText());
     }
 
 }
