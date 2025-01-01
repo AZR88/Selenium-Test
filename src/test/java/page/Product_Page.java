a61b7204b8427b0445f05bc10fab53b80ea46f44
AZR88 <azrielamaldany@gmail.com>
2025-01-01 22:26:05 +0700

    update Product_Page

---
+++ src/test/java/page/Product_Page.java
@@ -0,0 +1,12 @@
+package page;
+
+import org.openqa.selenium.By;
+
+public class Product_Page {
+
+    public static By ProductPrice = By.xpath("");
+    public static By ProductTitle = By.xpath("");
+    public static By ProductDesc = By.xpath("");
+    public static By ProductPic = By.xpath("");
+    public static By AddToCart= By.xpath("");
+}
