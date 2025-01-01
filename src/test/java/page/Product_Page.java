a5f1f08fb2cb9cd2a322b804d2f0f60236243936
AZR88 <azrielamaldany@gmail.com>
2025-01-01 23:17:04 +0700

    update Product_Page

--- src/test/java/page/Product_Page.java
+++ src/test/java/page/Product_Page.java
@@ -6,8 +6,8 @@ public class Product_Page {
 
 
     public static By ProductTitle = By.xpath("//h2[@class='name'");
-    public static By ProductPrice = By.xpath("//h3[@class='price-container']");
     public static By ProductPic = By.xpath("//div[@id='imgp']//img]");
+    public static By ProductPrice = By.xpath("//h3[@class='price-container']");
     public static By AddToCart= By.xpath("//a[@class='btn btn-success btn-lg' and text()='Add to cart']");
 
     public static By ProductDesc(String productdesc) {
