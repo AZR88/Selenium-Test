2fb2ab5b093db1eff092b88a834d90246c9a831e
AZR88 <azrielamaldany@gmail.com>
2025-01-02 16:20:09 +0700

    update Cart.java

--- src/test/java/page/Cart.java
+++ src/test/java/page/Cart.java
@@ -3,5 +3,11 @@ package page;
 import org.openqa.selenium.By;
 
 public class Cart {
-    public static By cartitem = By.xpath("");
+    public static By cartitem = By.xpath("//tbody[@id='tbodyid']//tr[@class='success']/td[2]");
+    public static By cartItemPrice = By.xpath("//tbody[@id='tbodyid']/tr[1]/td[3]");
+    public static By DeleteItem = By.xpath("//tbody[@id='tbodyid']//tr[@class='success']//a[contains(text(),'Delete')]");
+    public static By TotalPrice = By.id("totalp");
+
 }
+
+
