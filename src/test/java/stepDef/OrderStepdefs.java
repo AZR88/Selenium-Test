af34f0ff41545b9146020118ec5825b1241ceb36
AZR88 <azrielamaldany@gmail.com>
2025-01-03 20:59:33 +0700

    Create OrderStepdefs

---
+++ src/test/java/stepDef/OrderStepdefs.java
@@ -0,0 +1,10 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+
+public class OrderStepdefs {
+    @Given("I select the product {string}")
+    public void iSelectTheProduct(String arg0) {
+
+    }
+}
