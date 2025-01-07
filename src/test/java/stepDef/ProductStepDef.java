3190b8279e2b5b372e9867f1acb56244ba4d295a
AZR88 <azrielamaldany@gmail.com>
2025-01-07 23:43:17 +0700

    create ProductStepDef

---
+++ src/test/java/stepDef/ProductStepDef.java
@@ -0,0 +1,24 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import page.Item;
+import page.Product_Page;
+
+import static Helper.WebHelper.driver;
+public class ProductStepDef {
+    @When("the user click the product title {string}")
+    public void theUserClickTheProductTitle(String arg0) {
+        Item.selectItemByName(driver, arg0);
+    }
+
+    @Then("the product title should be {string}")
+    public void theProductTitleShouldBe(String arg0) {
+        Product_Page.checkTitle(driver, arg0);
+    }
+
+    @Then("the product price should be {string}")
+    public void theProductPriceShouldBe(String arg0) {
+        Product_Page.checkPrice();
+    }
+}
