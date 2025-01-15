a0017824514f11c66d574ff99268715dc41a0395
AZR88 <azrielamaldany@gmail.com>
2025-01-15 23:27:20 +0700

    Add fuction on CartStepdefs.java

--- src/test/java/stepDef/CartStepdefs.java
+++ src/test/java/stepDef/CartStepdefs.java
@@ -1,11 +1,15 @@
 package stepDef;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
 import page.Cart;
 import page.Item;
 import page.Product_Page;
+import java.util.List;
+import java.util.Map;
 
 import static Helper.WebHelper.driver;
 
@@ -35,6 +39,7 @@ public class CartStepdefs {
     public void theUserHasAddedTheItemToTheCart(String arg0) {
         Item.selectItemByName(driver, arg0);
         Product_Page.clickAdd(driver);
+        Product_Page.AlertDetect(driver);
     }
 
     @When("Click Cart button")
@@ -56,4 +61,70 @@ public class CartStepdefs {
     public void theUserDeletesTheItemFromTheCart() {
         Cart.Delete(driver);
     }
+
+    @Then("the item {string} should no longer be displayed in the cart")
+    public void theItemShouldNoLongerBeDisplayedInTheCart(String arg0) {
+        Cart.theItemShouldNoLongerBeDisplayedInTheCart(driver, arg0);
+    }
+    @And("total price should be {string}")
+    public void totalPriceShouldBe(String arg0) {
+    Cart.totalPrice(driver, arg0);
+    }
+
+
+    @Given("the user has added the following items to the cart:")
+
+    public void theUserHasAddedTheFollowingItemsToTheCart(DataTable dataTable) {
+        List<Map<String, String>> items = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> item : items) {
+            String itemName = item.get("Item Name");
+            Item.selectItemByName(driver, itemName);
+            Product_Page.clickAdd(driver);
+            Product_Page.AlertDetect(driver);
+            Item.ClickHomebutton(driver);
+        }
+    }
+
+
+
+    @Then("the total price displayed should match {string}")
+    public void theTotalPriceDisplayedShouldMatch(String arg0) {
+        Cart.CalculateTotalPrice(driver);
+    }
+
+
+    @Then("the user clicks the {string} button")
+    public void theUserClicksTheButton(String arg0) {
+        Cart.Order(driver);
+    }
+
+    @Then("the user fills in the following order details:")
+        public void theUserFillsInTheFollowingOrderDetails(DataTable dataTable) {
+            for (Map<String, String> row : dataTable.asMaps(String.class, String.class)) {
+                String fieldId = row.get("Field");  // ID field
+                String value = row.get("Value");    // Nilai yang akan dimasukkan ke dalam field
+
+                // Panggil metode fillField dari FormHelper untuk mengisi field
+                Cart.fillField(driver, fieldId, value);
+            }
+        }
+
+    @And("the user clicks the Purchase")
+    public void theUserClicksThePurchase() {
+        Cart.purchase(driver);
+    }
+
+    @Then("the user click ok button to complete the purchase")
+    public void theUserClickOkButtonToCompleteThePurchase() {
+        Cart.purchase(driver);
+    }
+
+    @Then("An alert Should be show up")
+    public void anAlertShouldBeShowUp() {
+        Product_Page.AlertDetect(driver);
+    }
 }
+
+
+
