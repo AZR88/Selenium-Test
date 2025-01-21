8e193e7c2f4eb8afc2c592b1c69b4cb113521d62
AZR88 <azrielamaldany@gmail.com>
2025-01-21 12:44:21 +0700

    update SignUpStepDef

--- src/test/java/stepDef/SignUpStepDef.java
+++ src/test/java/stepDef/SignUpStepDef.java
@@ -3,39 +3,57 @@ package stepDef;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import page.Login_Page;
 import page.SignUp_Page;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static Helper.WebHelper.driver;
 
 public class SignUpStepDef {
     @When("user click Signup button")
     public void userClickSignupButton() {
-        SignUp_Page.openSignupPage(driver);
+        WebElement button =SignUp_Page.openSignupPage(driver);
+        assertTrue(button.isDisplayed() && button.isEnabled());
+        button.click();
     }
 
     @And("user input username  with {string}")
     public void userInputUsernameWith(String arg0) {
-        SignUp_Page.SignUpusernameInput(driver, arg0);
+        WebElement inputUsername = SignUp_Page.SignUpusernameInput(driver);
+        String data = inputUsername.getAttribute("value");
+        assertTrue(inputUsername.isDisplayed() && inputUsername.isEnabled());
+        inputUsername.sendKeys(arg0);
+        assertEquals(arg0,data);
 
     }
 
     @And("user input password  with {string}")
     public void userInputPasswordWith(String arg0) {
-        SignUp_Page.SignUppasswordInput(driver,arg0);
+        WebElement inputpass = SignUp_Page.SignUppasswordInput(driver);
+        String data = inputpass.getAttribute("value");
+        assertTrue(inputpass.isDisplayed() && inputpass.isEnabled());
+        inputpass.sendKeys(arg0);
+        assertEquals(arg0,data);
     }
 
     @And("user click submit button")
     public void userClickSubmitButton() {
-        SignUp_Page.ClikSubmit(driver);
+        WebElement button = SignUp_Page.ClikSubmit(driver);
+        assertTrue(button.isDisplayed() && button.isEnabled());
+        button.click();
     }
 
     @Then("show account succes created alert")
     public void showAccountSuccesCreatedAlert() {
-        SignUp_Page.verifySignupAlert(driver);
-
+        boolean isAlertPresent = SignUp_Page.verifySignupAlert(driver);
+        Assert.assertTrue("No alert detected.", isAlertPresent);
     }
 
     @Then("show invalid SignUP notification")
     public void showInvalidSignUPNotification() {
-        SignUp_Page.verifySignupAlert(driver);
+        boolean isAlertPresent = SignUp_Page.verifySignupAlert(driver);
+        Assert.assertTrue("No alert detected.", isAlertPresent);
     }
 }
