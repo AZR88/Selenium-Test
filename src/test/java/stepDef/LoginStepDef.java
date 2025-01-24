ed8ede6b6c2321c244d42363cf55a8157b08a8d0
AZR88 <azrielamaldany@gmail.com>
2025-01-25 01:33:34 +0700

    update LoginUpStepDef.java

--- src/test/java/stepDef/LoginStepDef.java
+++ src/test/java/stepDef/LoginStepDef.java
@@ -6,8 +6,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-
 import page.Login_Page;
 import static Helper.WebHelper.driver;
 
@@ -16,42 +14,42 @@ public class LoginStepDef {
     @Given("user is on homepage {string}")
     public void userIsOnHomepage(String expectedUrl) {
         boolean isUrlCorrect = Login_Page.isUrlCorrect(driver, expectedUrl);
-        Assert.assertTrue("URL tidak sesuai!", isUrlCorrect);
+        Assert.assertTrue(" Incorrect URL !", isUrlCorrect);
     }
 
     @When("user click Login button")
     public void userClicksLoginButton() {
         boolean isLoginButtonClickable = Login_Page.clickLoginButton(driver);
-        Assert.assertTrue("Login button tidak bisa diklik atau tidak terlihat!", isLoginButtonClickable);
+        Assert.assertTrue("Login button Unclikable or not displayed", isLoginButtonClickable);
     }
 
     @And("user input username with {string}")
     public void userInputsUsernameWith(String username) {
         String inputtedUsername = Login_Page.inputUsername(driver, username);
-        Assert.assertEquals("Input username tidak sesuai!", username, inputtedUsername);
+        Assert.assertEquals("Input username Not Equal", username, inputtedUsername);
     }
 
     @And("user input password with {string}")
     public void userInputsPasswordWith(String password) {
         String inputtedPassword = Login_Page.inputPassword(driver, password);
-        Assert.assertEquals("Input password tidak sesuai!", password, inputtedPassword);
+        Assert.assertEquals("Input password Not Equal", password, inputtedPassword);
     }
 
     @And("user click submit")
     public void userClicksSubmitButton() {
         boolean isSubmitButtonClickable = Login_Page.clickSubmitButton(driver);
-        Assert.assertTrue("Submit button tidak bisa diklik atau tidak terlihat!", isSubmitButtonClickable);
+        Assert.assertTrue("Submit button Unclikable or not displayed!", isSubmitButtonClickable);
     }
 
     @Then("user redirect to home page with {string} username displayed")
     public void userRedirectedToHomePageWithUsernameDisplayed(String expectedUsername) {
         String actualUsername = Login_Page.getDisplayedUsername(driver);
-        Assert.assertEquals("Username yang ditampilkan tidak sesuai!", expectedUsername, actualUsername);
+        Assert.assertEquals("Username is not equals to account !", expectedUsername, actualUsername);
     }
 
     @Then("show invalid login notification")
     public void showInvalidLoginNotification() {
         boolean isAlertPresent = Login_Page.isLoginAlertPresent(driver);
-        Assert.assertTrue("Notifikasi login tidak valid tidak terdeteksi!", isAlertPresent);
+        Assert.assertTrue("invalid login notification detected!", isAlertPresent);
     }
 }
