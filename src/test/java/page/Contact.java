9aee7962cb737316584acab932227103c4a3ad5b
AZR88 <azrielamaldany@gmail.com>
2025-01-26 13:28:55 +0700

    fix Contact.java

--- src/test/java/page/Contact.java
+++ src/test/java/page/Contact.java
@@ -25,7 +25,7 @@ public class Contact {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement EmailField = wait.until(ExpectedConditions.visibilityOfElementLocated(SenderEmail));
         EmailField.sendKeys(email);
-        return EmailField.getAttribute("Value");
+        return EmailField.getAttribute("value");
     }
 
     public static String inputname(WebDriver driver, String name) {
