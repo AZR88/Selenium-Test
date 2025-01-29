a558aaf1edfbcdc3bc385a6603ed8017c874f328
AZR88 <azrielamaldany@gmail.com>
2025-01-30 01:47:45 +0700

    create Delete_Test.java

---
+++ src/test/java/Delete_Test.java
@@ -0,0 +1,19 @@
+import api.APITest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Delete_Test {
+    
+    @DataProvider(name = "userDeleteData")
+    public Object[][] createDeleteUserData() {
+        return new Object[][] {
+                { 204},  // Valid user ID, expects 204 (successful deletion)
+        };
+    }
+
+    @Test(dataProvider = "userDeleteData")
+    public void DeleteUser(int expectedStatusCode) {
+        APITest test = new APITest();
+        test.DeleteUser(expectedStatusCode);
+    }
+}
