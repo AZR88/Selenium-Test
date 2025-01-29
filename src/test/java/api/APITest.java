ab5612ff5a45e41aa11c9fa774fbec28252ecc51
AZR88 <azrielamaldany@gmail.com>
2025-01-29 15:18:44 +0700

    update APITest.java

---
+++ src/test/java/api/APITest.java
@@ -0,0 +1,156 @@
+package api;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class APITest {
+
+
+    public static void GetUserById(Integer ID, boolean shouldPass) {
+
+        RestAssured.baseURI = "https://reqres.in/";
+
+        if (shouldPass) {
+            given()
+                    .pathParam("id",ID)
+                    .when()
+                    .get("api/users?page=2")
+                    .then()
+                    .log().all()
+                    .assertThat().statusCode(200)
+                    .assertThat().body("data.id", Matchers.equalTo(ID));
+        } else {
+            given()
+                    .when()
+                    .get("api/users/" + ID)
+                    .then()
+                    .log().all()
+                    .assertThat().statusCode(404);
+        }
+    }
+
+
+
+    public void PostUser(String name, String job, boolean shouldPass) {
+        RestAssured.baseURI = "https://reqres.in/";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", name);
+        jsonObject.put("job", job);
+
+        if (shouldPass) {
+            given().log().all()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .body(jsonObject.toString())
+                    .post("/api/users")
+                    .then()
+                    .assertThat().statusCode(201)
+                    .assertThat().body("name", Matchers.equalTo(name))
+                    .assertThat().body("job", Matchers.equalTo(job))
+                    .assertThat().body("$", Matchers.hasKey("id"))
+                    .assertThat().body("$", Matchers.hasKey("createdAt"));
+        } else {
+            given().log().all()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .body(jsonObject.toString())
+                    .post("/api/users")
+                    .then()
+                    .assertThat().statusCode(400); // Status code untuk negative test
+        }
+    }
+
+
+
+    public static void  PutUser(Integer ID, String name, String job, boolean shouldPass){
+        RestAssured.baseURI = "https://reqres.in/";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userID", ID);
+        jsonObject.put("name", name);
+        jsonObject.put("job", job);
+
+        String fname = given().when().get("api/users/2"+ID).getBody().jsonPath().get("data.name");
+        String ljob = given().when().get("api/users/2"+ID).getBody().jsonPath().get("data.job");
+        System.out.println("name before ="+fname);
+
+
+
+        if (shouldPass){
+            given().log().all()
+                    .header("Content-Type","application/json")
+                    .body(jsonObject.toString())
+                    .put("api/users/2"+ID)
+                    .then().log().all()
+                    .assertThat().statusCode(200)
+                    .assertThat().body("name", Matchers.equalTo(name))
+                    .assertThat().body("job", Matchers.equalTo(job));}
+
+        else {
+            given().log().all()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .body(jsonObject.toString())
+                    .post("/api/users")
+                    .then()
+                    .assertThat().statusCode(400); // Status code untuk negative test}
+        }
+    }
+
+
+    public static void  PatchUser(Integer ID, String name, String job, boolean shouldPass){
+        RestAssured.baseURI = "https://reqres.in/";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userID", ID);
+        jsonObject.put("name", name);
+        jsonObject.put("job", job);
+
+
+        String fname = given().when().get("api/users/2"+ID).getBody().jsonPath().get("data.name");
+        String ljob = given().when().get("api/users/2"+ID).getBody().jsonPath().get("data.job");
+        System.out.println("name before ="+fname);
+
+
+
+        if (shouldPass){
+            given().log().all()
+                    .header("Content-Type","application/json")
+                    .body(jsonObject.toString())
+                    .put("api/users/2"+ID)
+                    .then().log().all()
+                    .assertThat().statusCode(200)
+                    .assertThat().body("name", Matchers.equalTo(name))
+                    .assertThat().body("job", Matchers.equalTo(job));}
+
+        else {
+            given().log().all()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .body(jsonObject.toString())
+                    .post("/api/users")
+                    .then()
+                    .assertThat().statusCode(400); // Status code untuk negative test}
+        }
+    }
+
+    @Test
+    public void DeleteUser(){
+        RestAssured.baseURI = "https://reqres.in/";
+
+        int userToDelete = 2;
+
+        given().log().all()
+                .when().delete("/api/users/2"+userToDelete)
+                .then()
+                .log().all()
+                .assertThat().statusCode(204);
+    }
+
+
+
+}
